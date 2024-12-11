c007454f489ff149ce4f4f41ef2deeb6262d3a7b
jarvisonline <98217852+jarvisonline@users.noreply.github.com>
2024-12-11 16:37:30 +0530

    lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
@@ -6,8 +6,20 @@ import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+import Lenis from "lenis";
 const App = () => {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis();
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
